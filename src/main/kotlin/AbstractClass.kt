be3c3de52d501122b8ee3f812af70b1ff6a34ffa
guruprasadDev <guruprasad.esambattu@mutualmobile.com>
2022-09-02 12:33:37 +0530

    Initial commit

---
+++ src/main/kotlin/AbstractClass.kt
@@ -0,0 +1,31 @@
+
+
+abstract class Employee(private val name:String, private val experience:Int){
+
+    abstract val salary :Double
+
+    abstract fun dateOfBirth(date:String)
+
+    fun employeeDetails(){
+        println("Employee name $name")
+        println("Employee Experience $experience")
+        println("Employee annual salary $salary")
+    }
+}
+class DeveloperAtMutualMobile(name:String,experience:Int):Employee(name,experience){
+
+    override val salary = 400000.0
+
+    override fun dateOfBirth(date:String){
+        println("Date of birth is $date")
+    }
+
+}
+fun main() {
+
+
+    val eng = DeveloperAtMutualMobile("E.Guru Prasad",3)
+    eng.employeeDetails()
+    eng.dateOfBirth("30/12/2000")
+
+}
---
+++ src/main/kotlin/kotlinFunction.kt
@@ -0,0 +1,4 @@
+fun student(name:String,rollNo:Int,course:String){
+
+    println("Name of the student ")
+}

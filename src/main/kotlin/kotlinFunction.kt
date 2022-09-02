5a1c4843d31d744083fdcf709d1f2879f9be4bfe
guruprasadDev <guruprasad.esambattu@mutualmobile.com>
2022-09-02 12:49:22 +0530

    initial Commit

--- src/main/kotlin/kotlinFunction.kt
+++ src/main/kotlin/kotlinFunction.kt
@@ -1,4 +1,27 @@
 fun student(name:String,rollNo:Int,course:String){
+    startingPointOfProgram()
+    println("Name of the student $name")
+    println("Student Roll Number $rollNo")
+    println("Name of the Course $course")
+    endingPointOfProgram()
 
-    println("Name of the student ")
+}
+
+fun main(){
+    student("E.Guru Prasad",7,"Bachelors of Computer Application")
+    student("E.Sai Kumar",8,"Bachelors of  Arts and Science")
+
+}
+
+fun startingPointOfProgram(){
+    println("")
+    println("Starting Point of Program")
+    println("*****************************")
+    println("")
+}
+
+fun endingPointOfProgram(){
+    println("")
+    println("*****************************")
+    println("Ending Point of Program")
 }

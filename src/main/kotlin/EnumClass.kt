1e3eb552c027f1abfe2a789ac6512fa088e74382
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-03 10:04:58 +0530

    initial commit

---
+++ src/main/kotlin/EnumClass.kt
@@ -0,0 +1,23 @@
+enum class Days(val isWeekEnd : Boolean = false){
+    SUNDAY(true),
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY(true);
+
+    companion object{
+        fun today(obj:Days):Boolean{
+            return obj.name.compareTo("SATURDAY")==0 || obj.name.compareTo("SUNDAY")==0
+
+        }
+
+    }
+}
+
+fun main(){
+    for(day in Days.values()){
+        println("${day.ordinal} = ${day.name} and is weekend ${day.isWeekEnd}")
+    }
+}

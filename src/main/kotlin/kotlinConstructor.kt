658c18794a9639b1c9aa3d48d5bdcc8cc7bea3ae
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-03 10:05:07 +0530

    initial commit

--- src/main/kotlin/kotlinConstructor.kt
+++ src/main/kotlin/kotlinConstructor.kt
@@ -1,6 +1,5 @@
 /** In Kotlin, one secondary constructor can call another secondary constructor of same class. This is done by using this() keyword.
 */
-
 class NewConstructor(password:String){
     constructor(name:String,id:Int,password: String):this(password){
         println("name of :- $name")
@@ -8,8 +7,6 @@ class NewConstructor(password:String){
         println("password :-$password")
     }
 }
-
 fun main(){
-
     var va = NewConstructor("Guru Prasad",333,"guru@#$1g")
 }

4b99a46c83c8a6bd6b9f823fb835e1831e5f148e
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-04 16:28:58 +0530

    initial commit

---
+++ src/main/kotlin/ExtensionFunction.kt
@@ -0,0 +1,22 @@
+class Student{
+    fun isPassed(mark:Int):Boolean{
+        return mark > 35
+    }
+}
+
+fun Student.isExcellent(mark:Int):Boolean{
+    return mark>90
+}
+
+fun Student.isFiled(mark:Int):Boolean{
+    return  mark < 35
+}
+
+fun main(){
+    val student = Student()
+    println("Student1 passing Status ${student.isPassed(45)}")
+    println("Student2 Excellent Status ${student.isExcellent(98)}")
+    println("Student3 filed Status ${student.isFiled(25)}")
+
+
+}
